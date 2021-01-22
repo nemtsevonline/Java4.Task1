0402d69ef64f471da2f0758638385a64633bef0f
Anton Nemtsev <nemtsev.online@gmail.com>
2021-01-22 15:44:37 +0300

    Add files

---
+++ BonusMilesService.java
@@ -0,0 +1,5 @@
+public class BonusMilesService {
+    public int calculate(int cost){
+        int miles = cost / 20;
+        return miles;
+}}
---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(10_000);
+        System.out.println(miles);
+    }
+}
